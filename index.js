a61f17c9b7aecfc938d9afdd8d2728b66a789c9e
David Tran <trandavidq@gmail.com>
2020-12-14 13:46:29 -0500

    added functionality and added feature to display db

---
+++ index.js
@@ -0,0 +1,79 @@
+const { ELOOP } = require('constants');
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const app = express();
+
+
+const port = 3000;
+
+function update_db(py_file_name){
+    //this function runs the py files located in /soccerLeagues in order to update the db with
+    //the current standings
+    const spawn = require('child_process').spawn;
+    const process = spawn('python',[__dirname+'/soccerLeagues/'+py_file_name]);
+    process.stdout.on('data',function(data){
+        //console.log(data.toString());
+    });
+}
+
+
+//homepage serving
+//updating db before any routing
+let laliga_data=[]
+let bundesliga_data=[];
+let epl_data=[];
+
+
+async function get_data(py_file_name,db_name,data_array,callback){
+    await callback(py_file_name);
+    let db = new sqlite3.Database('./'+db_name+'.db', sqlite3.OPEN_READONLY,(err)=>{
+        if(err){
+            console.log('error connecting to db');
+        }
+        else{
+            console.log('DB connection successful');
+        }
+    });
+    db.each("SELECT * from "+db_name,(err,row)=>{
+    
+        data_array.push(row);
+        console.log(row.team_name);
+        
+    });
+    db.close();
+    console.log(db_name+" data retrieved");
+}
+
+get_data('epl.py','epl_table',epl_data,update_db);
+get_data('laliga.py','laliga_table',laliga_data,update_db)
+get_data('bundesliga.py','bundesliga_table',bundesliga_data,update_db);
+
+
+app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.get('/epl',(req,res)=>{
+    console.log('EPL get request');
+    //epl_data has every row now, with all info
+    console.log(epl_data[1].team_name);
+    res.render('epl',{theData: epl_data});
+});
+
+app.get('/bundesliga',(req,res)=>{
+    console.log('Bundesliga get request');
+    console.log(bundesliga_data[1].team_name);
+    res.render('bundesliga',{theData: bundesliga_data});
+});
+
+app.get('/laliga',(req,res)=>{
+    console.log('La Liga get request');
+    //console.log(laliga_data[0].team_name);
+    res.render('laliga',{theData: laliga_data});
+    //res.send('debugging')
+});
+
+
+app.listen(port,(portNum)=> console.log(port+' listening'));
+
---
+++ soccerLeagues/bundesliga.py
@@ -0,0 +1,85 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+import os
+from os import path
+
+
+
+url_bundesliga = 'https://www.theguardian.com/football/bundesligafootball/table'
+r_bundesliga = requests.get(url_bundesliga)
+
+soup=BeautifulSoup(r_bundesliga.text,'html.parser')
+
+teams_bundesliga_dict = {};
+#grabs the html table (there is only one table)
+theTable = soup.find('table',class_='table table--football table--league-table table--responsive-font table--striped')
+
+
+def print_dict(teams_epl_dict):
+    for team in teams_epl_dict.keys():
+            print(team,end=" ")
+            for stat in teams_epl_dict[team]:
+                print(str(stat),end=" ")
+            print()
+
+#this for loop scrapes the data from the standings table
+def get_data(teams_epl_dict, theTable):
+    #initializes the dictionary with stats from the standings located on the guardian sports website
+    for epl_team in (theTable.find_all('tr',)[1:]):
+        team_name=epl_team.find('td',class_='table-column--main').text.strip()
+
+        #the stats in order: position, games played, wins, draws, losses, goals for
+        #goals against, goals differential, points
+        position = epl_team.find('td',class_='table-column--sub').text.strip()
+        games_played = epl_team.find_all('td')[2].text.strip()
+        wins = epl_team.find_all('td',class_='table-column--importance-1')[0].text.strip()
+        draws = epl_team.find_all('td',class_='table-column--importance-1')[1].text.strip()
+        losses = epl_team.find_all('td',class_='table-column--importance-1')[2].text.strip()
+        goal_differential = epl_team.find('td',class_='table-column--importance-3').text.strip()
+        points = epl_team.find_all('td')[9].text.strip()
+
+        teams_epl_dict[team_name]=[int(position),int(games_played),int(wins),int(draws),int(losses),int(goal_differential),int(points)]
+
+get_data(teams_bundesliga_dict,theTable)
+#print_dict(teams_epl_dict)
+
+
+#creating a database of the standings
+def create_and_initialize_db(tableName,teams_epl_dict):
+    print('Bundesliga creating')
+    connection = sqlite3.connect(tableName)
+    cursor = connection.cursor()
+
+    # table already created
+    cursor.execute("""CREATE TABLE IF NOT EXISTS bundesliga_table (
+                position INTEGER,
+                team_name TEXT UNIQUE,
+                games_played INTEGER,
+                wins INTEGER,
+                draws INTEGER,
+                losses INTEGER,
+                goal_differential INTEGER,
+                points INTEGER
+                )""")
+    
+
+    for t in teams_epl_dict.keys():
+        cursor.execute("INSERT OR IGNORE INTO bundesliga_table (position, team_name, games_played, wins, draws, losses, goal_differential, points) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        (teams_epl_dict[t][0],t,teams_epl_dict[t][1],teams_epl_dict[t][2],teams_epl_dict[t][3],teams_epl_dict[t][4],teams_epl_dict[t][5],teams_epl_dict[t][6]))
+        connection.commit()
+
+
+
+    cursor.close()
+    connection.close()
+
+def delete_db(tableName):
+    if(path.isfile(tableName)):
+        print('Bundesliga updating db')
+        os.remove(tableName)
+    else:
+        print('Bundesliga nothing to delete')
+
+delete_db('bundesliga_table.db')
+create_and_initialize_db('bundesliga_table.db',teams_bundesliga_dict)
